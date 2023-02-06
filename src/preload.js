d32ffd05feb7533b8a3ce26ad87a841d76d51f68
momo934 <ask4r.safiullin@yandex.ru>
2023-02-06 21:02:04 +0300

    move away from electron

--- src/preload.js
+++
@@ -1,6 +0,0 @@
-import { contextBridge, ipcRenderer } from "electron";
-
-contextBridge.exposeInMainWorld("timeline", {
-  days: (month, year) => ipcRenderer.invoke("timeline:getDays", month, year),
-  change: () => console.log("change"),
-});
